45664dd4709ff31c5f5ec7cc3c2ed75cd25d5e03
Alex <pincheota.5@gmail.com>
2025-12-01 22:05:54 -0300

    Cambios en la Pagina Home, al deslizar hacía abajo se actualiza la pagina principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ val camerax_version = "1.2.2"
 
 dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.transport.api)
     implementation(libs.transport.api)
     implementation(libs.transport.api)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
